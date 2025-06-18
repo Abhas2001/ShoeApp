cb6d16c19cbd0af902bdefdc3109a7bbfa33dae0
Abhas Kumar <abhassinha98@gmail.com>
2025-06-18 09:08:19 +0530

    design change

--- src/App.js
+++ src/App.js
@@ -93,12 +93,12 @@ setSuggestedRes(result.slice(0,5));
 
   return (
 
-    <section className="w-full h-screen bg-[#BFD6EB] overflow-y-auto overflow-x-hidden ">
+    <section className="w-full h-screen bg-[#F8F9FA] overflow-y-auto overflow-x-hidden ">
       <section className='flex justify-between items-center w-full'>
       <img alt='banner' src={t1}/>
       <span className="font-schoolbell text-lg">SNEAKER2SR</span>
 
-      <span>
+      <span className='mr-4'>
         <img alt='cart' src={cart}/>
       </span>
 
@@ -120,10 +120,10 @@ setSuggestedRes(result.slice(0,5));
         setSearch(true);
         }}
          onChange={(e)=>handleChange(e.target.value)}
-          placeholder={inputval.length===0?'Search':{inputval}}
+          placeholder={inputval.length===0?'Looking for Shoes':{inputval}}
           value={inputval}
           
-           className='w-[300px] border-2 px-4 py-[10px] rounded-md border-[#C5C5C5] bg-[#C5C5C5]'/>
+           className='w-[355px] border-2 px-4 py-[15px] rounded-3xl border-[#C5C5C5] bg-[#FFFFFF]'/>
           {/* autosuggestion */}
            {inputval.length>0&&!hideSuggest&&suggestedRes.map((x)=>{
             return(
@@ -153,10 +153,10 @@ setSuggestedRes(result.slice(0,5));
         </button>
       </section>
 
-      <section className={`ml-6 mt-6 flex gap-4 shrink-0 overflow-x-auto ${inputval.length>0&&'hidden'} `}>
+      <section className={`ml-6 mt-2 flex gap-4 shrink-0 overflow-x-auto ${inputval.length>0&&'hidden'} `}>
         {Brands.map((x)=>{
           return (
-            <div onClick={(e)=>handleSelected(e.target.innerText)} className={` w-[130px] h-[50px] rounded-lg border-2 border-[#292929] ${selected===x?'bg-[#292929]':'bg-[#BFD6EB]'} flex justify-center items-center shrink-0`}>
+            <div onClick={(e)=>handleSelected(e.target.innerText)} className={` w-[130px] h-[50px] rounded-lg border-2 border-[#fff] ${selected===x?'bg-[#292929]':'bg-[#ffffff]'} flex justify-center items-center shrink-0`}>
             <span  className={`font-bold text-[20px] ${selected===x?'text-[#ffffff]':'text-[#9B9B9B]'}`}>
 
                {x==='HUSHPUPPIES'?'HUSHP':x}
@@ -168,9 +168,9 @@ setSuggestedRes(result.slice(0,5));
 }
       </section>
 
-      <section className={`mt-4 ml-6 w-full flex gap-36 items-center ${inputval.length>0&&search&&'hidden'}`}>
+      <section className={`mt-4 ml-6 w-full flex gap-[115px] items-center ${inputval.length>0&&search&&'hidden'}`}>
         <span className='text-[25px] font-bold text-[#000000]'>
-          New Arrival
+          Popular Shoes
         </span>
 
         <button className='text-[#828282]'>
@@ -183,7 +183,7 @@ setSuggestedRes(result.slice(0,5));
         {inputval.length>0&&search?
 
         searchedRes.length>0?
-         <section className='mt-4 ml-6 grid grid-cols-2 gap-4 overflow-y-auto'>
+         <section className='mt-4 ml-6 flex gap-4 overflow-x-auto'>
                  {searchedRes.map((value)=>{
                   return(
                     <section>
@@ -217,7 +217,7 @@ setSuggestedRes(result.slice(0,5));
               <img className='w-24 h-24' alt='loader' src={loader}/>
              </div>
         :
-      <section className='mt-4 mb-12 ml-6 grid grid-cols-2 gap-4 '>
+      <section className='mt-4 ml-6 flex gap-4 overflow-x-auto '>
 
       {Object.values(data).map((value)=>{
         
@@ -225,12 +225,12 @@ setSuggestedRes(result.slice(0,5));
     return(
    
      
-        <section className='mb-4 '>
-      <div className='w-[157px] h-[157px] bg-[#FFFFFF] rounded-md flex justify-center items-center shrink-0 shadow-2xl'>
+        <section >
+      <div className='w-[157px] h-[201px] bg-[#f5f5f5] border-[#fff] rounded-tl-2xl rounded-br-2xl flex flex-col justify-center items-center shrink-0 shadow-lg '>
       <img alt='value' className='w-[137px] h-[75px] object-cover'  src={value.imageURL}/>
-    </div>
+    
 
-    <div>
+    <div className='mt-6 flex flex-col gap-1'>
       <div className='w-[137px] truncate overflow-hidden whitespace-nowrap '>
         <span  className='text-[16px] font-bold'>
           {value.name}
@@ -240,6 +240,7 @@ setSuggestedRes(result.slice(0,5));
         <span className='font-medium text-[16px] text-[#FF0000]'>
           {`$ ${value.price}`}
         </span>
+    </div>
     </div>
     </section>
   
@@ -255,7 +256,31 @@ setSuggestedRes(result.slice(0,5));
       
 }
       </section>
-   
+
+      <section className={`ml-6 w-full flex gap-[130px] items-center ${inputval.length>0&&search&&'hidden'}`}>
+        <span className='text-[25px] font-bold text-[#000000]'>
+         New Arrivals
+        </span>
+
+        <button className='text-[#828282]'>
+          View all
+        </button>
+      </section>
+         
+
+         <section className='ml-5' >
+         <div className='w-[345px] h-[201px] bg-[#ffffff] border-[#fff] rounded-tl-2xl rounded-br-2xl flex gap-4 justify-center items-center shrink-0 shadow-lg '>
+           <div className='flex flex-col'>
+            <span>Best Choice</span>
+            <span>Nike</span>
+            <span className='mt-2'>$130</span>
+           </div>
+           <div>
+            <img alt='text' className='w-[137px] h-[75px] object-cover' src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-665455a5-45de-40fb-945f-c1852b82400d/react-infinity-run-flyknit-mens-running-shoe-zX42Nc.jpg" />
+           </div>
+          </div>
+
+         </section>
 
   
 
