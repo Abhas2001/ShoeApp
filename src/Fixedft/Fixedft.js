3dd3add323e9413804c55754ec026ea82fa567c4
Abhas Kumar <abhassinha98@gmail.com>
2025-06-24 23:44:33 +0530

    ui change

--- src/Fixedft/Fixedft.js
+++ src/Fixedft/Fixedft.js
@@ -2,7 +2,7 @@ import React from 'react'
 import centercart from '../../src/images/centercart.svg'
 import home from '../../src/images/home.svg'
 import heart from '../../src/images/heart.svg'
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 
 
@@ -11,18 +11,21 @@ const Fixedft = () => {
     
   const navigate = useNavigate();
     const handleCart = () =>{
+
         navigate("/cart")
      }
 
-     const handleHome = () =>{
-        navigate("/")
-     }
+    //  const handleHome = () =>{
+    //     navigate("/")
+    //  }
   return (
    
     <section className='w-full  h-12 bg-[#ffffff] flex justify-evenly items-center fixed bottom-0 p-4'>
-      <div onClick={handleHome}>
+        <Link to="/">
+      <div >
         <img alt='home' src={home}/>
       </div>
+      </Link>
       <div onClick={handleCart}>
       <img alt='cart' src={centercart}/>
       </div>
